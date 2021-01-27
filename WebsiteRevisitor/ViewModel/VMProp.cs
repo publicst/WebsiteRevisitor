c90c06640410202220cf77407fb62f2c60a46bd8
publicst <publicst119@gmail.com>
2021-01-27 13:31:21 -0800

    Adding templated property for VM.

---
+++ WebsiteRevisitor/ViewModel/VMProp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebsiteRevisitor.ViewModel
+{
+    public class VMProp<T> : ViewModelBase
+    {
+        private T _value;
+        public T Value
+        {
+            get => _value;
+            set { _value = value; RaisePropertyChanged(); }
+        }
+    }
+}
