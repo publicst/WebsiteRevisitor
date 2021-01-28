03e125bf664dc90899b8aa6e99087f67ba5e3144
publicst <publicst119@gmail.com>
2021-01-28 09:42:15 -0800

    Current culture is fine.

--- WebsiteRevisitor/ViewModel/MainWindowViewModel.cs
+++ WebsiteRevisitor/ViewModel/MainWindowViewModel.cs
@@ -116,7 +116,7 @@ namespace WebsiteRevisitor
             _websiteCollectionView.SortDescriptions.Add(new SortDescription("LastChecked", ListSortDirection.Descending));
         }
 
-        private void UpdateWindowTitle() => WindowTitle.Value = $"Website Revisitor {DateTime.Now.ToString("MMM/dd/yyyy dddd HH:mm:ss", new System.Globalization.CultureInfo("en-US"))}";
+        private void UpdateWindowTitle() => WindowTitle.Value = $"Website Revisitor {DateTime.Now:MMM/dd/yyyy dddd HH:mm:ss}";
         #endregion
 
         #region Dispatcher Timer related codes
